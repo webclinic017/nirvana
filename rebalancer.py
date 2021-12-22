de9de295bc419fe58bf9ff7494ee5ce62af1dc8b
Taylor Ralph <tralph@github.com>
2021-12-21 22:06:00 -0600

    Major refactor

--- rebalancer.py
+++ rebalancer.py
@@ -6,16 +6,13 @@ class Rebalancer:
         self.absolute_rebalance = False
         self.relative_rebalance = False
 
-    def rebalance_check(self, current, target):
+    def rebalance_check(self, cash, current, target):
         # calculate position and account_value
         # print(current)
         # print(target)
-        self.account_value = 0
+        self.account_value = cash
         for position in current:
-            # print(position)
-            if position != 'CASHX':
-                current[position]['value'] = current[position]['shares'] * current[position]['last_price']
-
+            current[position]['value'] = current[position]['shares'] * current[position]['last_price']
             self.account_value += current[position]['value']
         
         # check if need to reblance
@@ -23,7 +20,7 @@ class Rebalancer:
         self.relative_rebalance = False
 
         for position in current:
-            if target[position] == 0 or position == 'CASHX':
+            if target[position] == 0:
                 continue
             current[position]['percent'] = current[position]['value'] / self.account_value
             deviation = abs(current[position]['percent'] - target[position] / 100)
@@ -45,8 +42,6 @@ class Rebalancer:
         trades = {}
 
         for position in current:
-            if position == 'CASHX':
-                continue
             # print(target[position])
             target_value = self.account_value * target[position] / 100
             current_value = current[position]['value']
--- strategies.py
+++ strategies.py
@@ -33,35 +33,42 @@ class Nirvana(bt.Strategy):
         self.dataclose = self.datas[0].close
         self.inout = 0
         self.order = None
-
-        self.df_spy = pd.read_csv("history/SPY.csv").set_index("Date")
-        self.df_spxl = pd.read_csv("history/SPXL.csv").set_index("Date")
-        self.df_tqqq = pd.read_csv("history/TQQQ.csv").set_index("Date")
-        self.df_gbtc = pd.read_csv("history/GBTC.csv").set_index("Date")
-        self.df_ethe = pd.read_csv("history/ETHE.csv").set_index("Date")
-        self.df_gld = pd.read_csv("history/GLD.csv").set_index("Date")
-        self.df_ugl = pd.read_csv("history/UGL.csv").set_index("Date")
-        self.df_tlt = pd.read_csv("history/TLT.csv").set_index("Date")
-        self.df_tmf = pd.read_csv("history/TMF.csv").set_index("Date")
-        self.df_tsla = pd.read_csv("history/TSLA.csv").set_index("Date")
-
         if not self.p.optimizer:
             self.rows = []
             self.rows.append("Date,Transaction,Ticker,Shares,Price")
         self.buy_price = {}
         self.first_run = True
-
         if (self.p.tearsheet):
             self.portvalue = self.broker.getvalue()
             self.gains = []
             self.dates = []
 
         self.target = self.p.portin.copy()
-        self.target['CASHX'] = 0
-        print(self.target)
         self.target_update = self.target.copy()
+
+        self.ticker = {}
+        self.ma_above = {}
         self.portfolio = {}
+        self.df_history = {}
+        for symbol in self.target:
+            self.df_history[symbol] = pd.read_csv("history/" + symbol + ".csv").set_index("Date")
+
+        # create rebalancer and set absolute and relative deviation limits
         self.rb = rebalancer.Rebalancer(absolute_deviation_limit = 0.05, relative_deviation_limit = 0.25)
+
+        # set moving average type and period plus upper and lower limits
+        self.ma_limits = {
+            'SPY':   {'ma': 'SMA_200', 'upper': 1.04, 'lower': 0.95},
+            'SPXL':  {'ma': 'SMA_200', 'upper': 1.05, 'lower': 0.90},
+            'TQQQ':  {'ma': 'SMA_180', 'upper': 1.03, 'lower': 0.95},
+            'TLT':   {'ma': 'SMA_180', 'upper': 1.05, 'lower': 0.90},
+            'TMF':   {'ma': 'SMA_180', 'upper': 1.05, 'lower': 0.90},
+            'GBTC':  {'ma': 'SMA_50',  'upper': 1.05, 'lower': 0.90},
+            'ETHE':  {'ma': 'SMA_100', 'upper': 1.05, 'lower': 0.90},
+            'GLD':   {'ma': 'SMA_180', 'upper': 1.05, 'lower': 0.90},
+            'UGL':   {'ma': 'SMA_180', 'upper': 1.05, 'lower': 0.90},
+            'TSLA':  {'ma': 'SMA_180', 'upper': 1.05, 'lower': 0.90}
+        }
 
     def notify_order(self, order):
         debug = False
@@ -91,194 +98,52 @@ class Nirvana(bt.Strategy):
         date_dt = self.datas[0].datetime.date(0)
         date = date_dt.strftime('%Y-%m-%d')
 
-        if 'SPY' in self.target:
-            spy_price = self.df_spy.loc[date]['Adj Close']
-            spy_sma = self.df_spy.loc[date]['SMA_200']
-        if 'SPXL' in self.target:
-            spxl_price = self.df_spxl.loc[date]['Adj Close']
-            spxl_sma = self.df_spxl.loc[date]['SMA_200']
-        if 'TQQQ' in self.target:
-            tqqq_price = self.df_tqqq.loc[date]['Adj Close']
-            tqqq_sma = self.df_tqqq.loc[date]['SMA_180']
-        if 'TLT' in self.target:
-            tlt_price = self.df_tlt.loc[date]['Adj Close']
-            tlt_sma = self.df_tlt.loc[date]['SMA_180']
-        if 'TMF' in self.target:
-            tmf_price = self.df_tmf.loc[date]['Adj Close']
-            tmf_sma = self.df_tmf.loc[date]['SMA_180']
-        if 'GBTC' in self.target:
-            gbtc_price = self.df_gbtc.loc[date]['Adj Close']
-            gbtc_sma = self.df_gbtc.loc[date]['SMA_50']
-        if 'ETHE' in self.target:
-            ethe_price = self.df_ethe.loc[date]['Adj Close']
-            ethe_sma = self.df_ethe.loc[date]['SMA_100']
-        if 'GLD' in self.target:
-            gld_price = self.df_gld.loc[date]['Adj Close']
-            gld_sma = self.df_gld.loc[date]['SMA_180']
-        if 'UGL' in self.target:
-            ugl_price = self.df_ugl.loc[date]['Adj Close']
-            ugl_sma = self.df_ugl.loc[date]['SMA_180']
-        if 'TSLA' in self.target:
-            tsla_price = self.df_tsla.loc[date]['Adj Close']
-            tsla_sma = self.df_tsla.loc[date]['SMA_180']
-
-        if self.first_run:
-            if 'SPY' in self.target:
-                self.spy_sma_above = spy_price >= spy_sma * 1.0
-                if not self.spy_sma_above:
-                    self.target_update['SPY'] = 0
-            if 'SPXL' in self.target:
-                self.spxl_sma_above = spxl_price >= spxl_sma * 1.0
-                if not self.spxl_sma_above:
-                    self.target_update['SPXL'] = 0
-            if 'TQQQ' in self.target:
-                self.tqqq_sma_above = tqqq_price >= tqqq_sma * 1.0
-                if not self.tqqq_sma_above:
-                    self.target_update['TQQQ'] = 0
-            if 'TLT' in self.target:
-                self.tlt_sma_above = tlt_price >= tlt_sma * 1.0
-                if not self.tlt_sma_above:
-                    self.target_update['TLT'] = 0
-            if 'TMF' in self.target:
-                self.tmf_sma_above = tmf_price >= tmf_sma * 1.0
-                if not self.tmf_sma_above:
-                    self.target_update['TMF'] = 0
-            if 'GBTC'in self.target:
-                self.gbtc_sma_above = gbtc_price >= gbtc_sma * 1.0
-                if not self.gbtc_sma_above:
-                    self.target_update['GBTC'] = 0
-            if 'ETHE' in self.target:
-                self.ethe_sma_above = ethe_price >= ethe_sma * 1.0
-                if not self.ethe_sma_above:
-                    self.target_update['ETHE'] = 0
-            if 'GLD' in self.target:
-                self.gld_sma_above = gld_price >= gld_sma * 1.0
-                if not self.gld_sma_above:
-                    self.target_update['GLD'] = 0
-            if 'UGL' in self.target:
-                self.ugl_sma_above = ugl_price >= ugl_sma * 1.0
-                if not self.ugl_sma_above:
-                    self.target_update['UGL'] = 0
-            if 'TSLA' in self.target:
-                self.tsla_sma_above = tsla_price >= tsla_sma * 1.0
-                if not self.tsla_sma_above:
-                    self.target_update['TSLA'] = 0
-
-        current = {}
+        # update daily closing price and moving average
         for symbol in self.target:
-            current[symbol] = {'shares': 0, 'last_price': 0}
-        
-        current['CASHX'] = {'value': self.broker.getcash()}
+            self.ticker[symbol] = {'price': self.df_history[symbol].loc[date]['Adj Close'], 'ma': self.df_history[symbol].loc[date][self.ma_limits[symbol]['ma']]}
 
-        # Update current portfolio allocation
+        # determine if we are above the upper range of moving average at the start
+        if self.first_run:
+            for symbol in self.target:
+                self.ma_above[symbol] = self.ticker[symbol]['price'] >= self.ticker[symbol]['ma'] * 1.0
+                if not self.ma_above[symbol]:
+                    self.target_update[symbol] = 0
+
+        rebalance_ma = False
+
+        # check if price crossed moving average
+        for symbol in self.target:
+            if (self.ticker[symbol]['price'] < self.ticker[symbol]['ma'] * self.ma_limits[symbol]['lower'] and self.ma_above[symbol]):
+                self.target_update[symbol] = 0
+                self.ma_above[symbol] = False
+                rebalance_ma = True
+            elif (self.ticker[symbol]['price'] >= self.ticker[symbol]['ma'] * self.ma_limits[symbol]['upper'] and not self.ma_above[symbol]):
+                self.target_update[symbol] = self.target[symbol]
+                self.ma_above[symbol] = True
+                rebalance_ma = True
+
+        # update cash at broker
+        cash = self.broker.getcash()
+
+        # update ortfolio holdings at broker
         for data in self.datas:
             if (data._name in self.target):
-                current[data._name]['shares'] = self.broker.getposition(data).size
-                current[data._name]['last_price'] = data.close[0]
+                self.portfolio[data._name] = {'shares': self.broker.getposition(data).size, 'last_price':data.close[0]}
 
-        rebalance_sma = False
-
-        if 'SPY' in self.target:
-            if (spy_price < spy_sma * 0.95 and self.spy_sma_above):
-                self.target_update['SPY'] = 0
-                rebalance_sma = True
-                self.spy_sma_above = False
-            elif (spy_price >= spy_sma * 1.04 and not self.spy_sma_above):
-                self.target_update['SPY'] = self.target['SPY']
-                rebalance_sma = True
-                self.spy_sma_above = True
-        if 'SPXL' in self.target:
-            if (spxl_price < spxl_sma * 0.90 and self.spxl_sma_above):
-                self.target_update['SPXL'] = 0
-                rebalance_sma = True
-                self.spxl_sma_above = False
-            elif (spxl_price >= spxl_sma * 1.05 and not self.spxl_sma_above):
-                self.target_update['SPXL'] = self.target['SPXL']
-                rebalance_sma = True
-                self.spxl_sma_above = True
-        if 'TQQQ' in self.target:
-            if (tqqq_price < tqqq_sma * 0.95 and self.tqqq_sma_above):
-                self.target_update['TQQQ'] = 0
-                rebalance_sma = True
-                self.tqqq_sma_above = False
-            elif (tqqq_price >= tqqq_sma * 1.03 and not self.tqqq_sma_above):
-                self.target_update['TQQQ'] = self.target['TQQQ']
-                rebalance_sma = True
-                self.tqqq_sma_above = True
-        if 'TLT' in self.target:
-            if (tlt_price < tlt_sma * 0.90 and self.tlt_sma_above):
-                self.target_update['TLT'] = 0
-                rebalance_sma = True
-                self.tlt_sma_above = False
-            elif (tlt_price >= tlt_sma * 1.05 and not self.tlt_sma_above):
-                self.target_update['TLT'] = self.target['TLT']
-                rebalance_sma = True
-                self.tlt_sma_above = True
-        if 'TMF' in self.target:
-            if (tmf_price < tmf_sma * 0.90 and self.tmf_sma_above):
-                self.target_update['TMF'] = 0
-                rebalance_sma = True
-                self.tmf_sma_above = False
-            elif (tmf_price >= tmf_sma * 1.05 and not self.tmf_sma_above):
-                self.target_update['TMF'] = self.target['TMF']
-                rebalance_sma = True
-                self.tmf_sma_above = True
-        if 'GBTC' in self.target:
-            if (gbtc_price < gbtc_sma * 0.9 and self.gbtc_sma_above):
-                self.target_update['GBTC'] = 0
-                rebalance_sma = True
-                self.gbtc_sma_above = False
-            elif (gbtc_price >= gbtc_sma * 1.05 and not self.gbtc_sma_above):
-                self.target_update['GBTC'] = self.target['GBTC']
-                rebalance_sma = True
-                self.gbtc_sma_above = True
-        if 'ETHE' in self.target:
-            if (ethe_price < ethe_sma * 0.9 and self.ethe_sma_above):
-                self.target_update['ETHE'] = 0
-                rebalance_sma = True
-                self.ethe_sma_above = False
-            elif (ethe_price >= ethe_sma * 1.05 and not self.ethe_sma_above):
-                self.target_update['ETHE'] = self.target['ETHE']
-                rebalance_sma = True
-                self.ethe_sma_above = True
-        if 'GLD' in self.target:
-            if (gld_price < gld_sma * 0.9 and self.gld_sma_above):
-                self.target_update['GLD'] = 0
-                rebalance_sma = True
-                self.gld_sma_above = False
-            elif (gld_price >= gld_sma * 1.05 and not self.gld_sma_above):
-                self.target_update['GLD'] = self.target['GLD']
-                rebalance_sma = True
-                self.gld_sma_above = True
-        if 'UGL' in self.target:
-            if (ugl_price < ugl_sma * 0.9 and self.ugl_sma_above):
-                self.target_update['UGL'] = 0
-                rebalance_sma = True
-                self.ugl_sma_above = False
-            elif (ugl_price >= ugl_sma * 1.05 and not self.ugl_sma_above):
-                self.target_update['UGL'] = self.target['UGL']
-                rebalance_sma = True
-                self.ugl_sma_above = True
-        if 'TSLA' in self.target:
-            if (tsla_price < tsla_sma * 0.90 and self.tsla_sma_above):
-                self.target_update['TSLA'] = 0
-                rebalance_sma = True
-                self.tsla_sma_above = False
-            elif (tsla_price >= tsla_sma * 1.05 and not self.tsla_sma_above):
-                self.target_update['TSLA'] = self.target['TSLA']
-                rebalance_sma = True
-                self.tsla_sma_above = True
-
-        rebalance = self.rb.rebalance_check(current, self.target_update)
+        # check if any positions in portfolio are outside rebalancing bands
+        rebalance_bands = self.rb.rebalance_check(cash, self.portfolio, self.target_update)
         
-        if (rebalance or rebalance_sma or self.first_run):
-            print("rebalance: " + str(rebalance) + " rebalance_sma: " + str(rebalance_sma))
-            print("Cash = " + str(current['CASHX']))
-            trades = self.rb.rebalance(current, self.target_update)
+        if (rebalance_bands or rebalance_ma or self.first_run):
+            print("rebalance_bands: " + str(rebalance_bands) + " rebalance_ma: " + str(rebalance_ma))
+            print("cash = " + str(cash))
 
+            # generate trades to rebalance back to the updated target allocation
+            trades = self.rb.rebalance(self.portfolio, self.target_update)
+
+            # process sell orders
             for data in self.datas:
                 if (data._name in trades and trades[data._name]['action'] == 'SELL'):
-                    size = math.ceil(trades[data._name]['amount'] / current[data._name]['last_price'])
+                    size = math.ceil(trades[data._name]['amount'] / self.portfolio[data._name]['last_price'])
                     if size > self.broker.getposition(data).size:
                         size = self.broker.getposition(data).size
                     if int(size) < 1:
@@ -290,10 +155,11 @@ class Nirvana(bt.Strategy):
                         print("{}: SELL {} {} shares at {}".format(
                             date, "%5s" % data._name,"%9d" % self.order.size, "%7.2f" % data.close[0]))
                         self.rows.append("{},{},{},{},{}".format(date, "SELL", data._name, self.order.size, data.close[0]))
-    
+
+            # process buy orders
             for data in self.datas:
                 if (data._name in trades and trades[data._name]['action'] == 'BUY'):
-                    size = math.floor(trades[data._name]['amount'] / current[data._name]['last_price'])
+                    size = math.floor(trades[data._name]['amount'] / self.portfolio[data._name]['last_price'])
                     if size == 0:
                         continue
 
